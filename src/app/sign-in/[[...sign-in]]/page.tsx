fa9ee590245568ccfe70c26c37ab765ed4cbf24f
shoma883 <shouma.s.0509@gmail.com>
2025-07-11 17:16:19 +0900

    Clerk authentication success 2

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,8 @@
-import { SignIn } from '@clerk/nextjs'
+// src/app/sign-in/[[...sign-in]]/page.tsx
+
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />
+  return <SignIn aftersigninUrl="/dashboard" />
+  ;
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
-import { SignUp } from '@clerk/nextjs'
+// src/app/sign-up/[[...sign-up]]/page.tsx
+
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />
+  return <SignUp aftersignoutUrl="/dashboard" />;
 }
