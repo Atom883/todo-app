61fc1802f3e74b65023786466ef90da18f714a8d
shoma883 <shouma.s.0509@gmail.com>
2025-07-11 15:50:49 +0900

    initial commit

---
+++ src/lib/prisma.ts
@@ -0,0 +1,34 @@
+// // src/lib/prisma.ts
+// import { PrismaClient } from '@prisma/client'
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ['query', 'info', 'warn', 'error'],
+//   })
+
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma
+// }
+
+
+
+
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
